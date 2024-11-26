1c23c71807610c9e52b8ddfcd8d59dd56a70c587
danicabi <daniela.cab39@gmail.com>
2024-11-26 00:11:53 -0600

    level 1 done. Interactable safe and tutorial done

--- Assets/endGameBehavoir.cs
+++ Assets/endGameBehavoir.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class endGameBehavoir : MonoBehaviour
 {
     public playerInputHandler playermvoemtn;
+    bool allowOpen;
+
+    public void Update(){
+        if(allowOpen && Input.GetKeyDown(KeyCode.E)){
+            Open();
+        }
+    }
     
     public void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
-            playermvoemtn.enabled = false;
-            LevelManager.instance.winner();
+            allowOpen = true;
         }
+    }
+
+    public void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            allowOpen = false;
+        }
+    }
+
+    public void Open(){
+        playermvoemtn.enabled = false;
+        LevelManager.instance.winner();
     }
 }
